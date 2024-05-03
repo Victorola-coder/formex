9dc7c2ea44fc040179b75b7ceea09d687faea0ae
Victor Olatunji <victoluolatunji@gmail.com>
2024-05-03 12:13:08 +0100

    [add]: fields

---
+++ components/FormElements.tsx
@@ -0,0 +1,75 @@
+import { CheckboxFieldFormElement } from "./fields/checkbox";
+import { DateFieldFormElement } from "./fields/date";
+import { NumberFieldFormElement } from "./fields/number";
+import { ParagprahFieldFormElement } from "./fields/paragraph";
+import { SelectFieldFormElement } from "./fields/select";
+import { SeparatorFieldFormElement } from "./fields/seperator";
+import { SpacerFieldFormElement } from "./fields/spacer";
+import { SubTitleFieldFormElement } from "./fields/subTitle";
+import { TextAreaFormElement } from "./fields/textArea";
+import { TextFieldFormElement } from "./fields/text";
+import { TitleFieldFormElement } from "./fields/title";
+
+export type ElementsType =
+  | "TextField"
+  | "TitleField"
+  | "SubTitleField"
+  | "ParagraphField"
+  | "SeparatorField"
+  | "SpacerField"
+  | "NumberField"
+  | "TextAreaField"
+  | "DateField"
+  | "SelectField"
+  | "CheckboxField";
+
+export type SubmitFunction = (key: string, value: string) => void;
+
+export type FormElement = {
+  type: ElementsType;
+
+  construct: (id: string) => FormElementInstance;
+
+  designerBtnElement: {
+    icon: React.ElementType;
+    label: string;
+  };
+
+  designerComponent: React.FC<{
+    elementInstance: FormElementInstance;
+  }>;
+  formComponent: React.FC<{
+    elementInstance: FormElementInstance;
+    submitValue?: SubmitFunction;
+    isInvalid?: boolean;
+    defaultValue?: string;
+  }>;
+  propertiesComponent: React.FC<{
+    elementInstance: FormElementInstance;
+  }>;
+
+  validate: (formElement: FormElementInstance, currentValue: string) => boolean;
+};
+
+export type FormElementInstance = {
+  id: string;
+  type: ElementsType;
+  extraAttributes?: Record<string, any>;
+};
+
+type FormElementsType = {
+  [key in ElementsType]: FormElement;
+};
+export const FormElements: FormElementsType = {
+  TextField: TextFieldFormElement,
+  TitleField: TitleFieldFormElement,
+  SubTitleField: SubTitleFieldFormElement,
+  ParagraphField: ParagprahFieldFormElement,
+  SeparatorField: SeparatorFieldFormElement,
+  SpacerField: SpacerFieldFormElement,
+  NumberField: NumberFieldFormElement,
+  TextAreaField: TextAreaFormElement,
+  DateField: DateFieldFormElement,
+  SelectField: SelectFieldFormElement,
+  CheckboxField: CheckboxFieldFormElement,
+};
