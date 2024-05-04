6587794682caa5d450d9c645f794caeb6718fecf
Victor Olatunji <victoluolatunji@gmail.com>
2024-05-04 05:02:49 +0100

    clerk shit lol

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,14 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
-  return (
-    <section
-      className={`flex flex-col items-center justify-center min-h-screen p-3 gap-3`}
-    >
-      <div className="text-xl font-semibold sm:text-2xl">
-        Login to your Formex account
-      </div>
-      <SignIn />
-    </section>
-  );
+  return <SignIn />;
 }
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="flex flex-row min-h-screen">
-      <h2>Home</h2>
-    </main>
-  );
-}
