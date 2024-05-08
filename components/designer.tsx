7ace774d31632b0253fb6dc4ce6de90a832e00ae
Victot VickyJay <victoluolatunji@gmail.com>
2024-05-08 12:07:40 +0100

    progress on designer

--- components/designer.tsx
+++ components/designer.tsx
@@ -1,5 +1,56 @@
 "use client"
 
+import { cn } from "@/lib/utils";
+import {
+    DragEndEvent,
+    useDndMonitor,
+    useDraggable,
+    useDroppable,
+  } from "@dnd-kit/core";
+
+
 export default function Designer(){
-    return <div>Designer </div>
+    const droppable = useDroppable({
+        id: "designer-drop-area",
+        data: {
+          isDesignerDropArea: true,
+        },
+      });
+
+    return  <div className="flex w-full h-full">
+    <div
+      className="p-4 w-full"
+    //   onClick={() => {
+    //     if (selectedElement) setSelectedElement(null);
+    //   }}
+    >
+      <div
+        ref={droppable.setNodeRef}
+        className={cn(
+          "bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto",
+          droppable.isOver && "ring-4 ring-primary ring-inset"
+        )}
+      >
+        {/* {!droppable.isOver && elements.length === 0 && (
+        )} */}
+          <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
+            Drop here
+          </p>
+
+        {droppable.isOver && elements.length === 0 && (
+          <div className="p-4 w-full">
+            <div className="h-[120px] rounded-md bg-primary/20"></div>
+          </div>
+        )}
+        {elements.length > 0 && (
+          <div className="flex flex-col  w-full gap-2 p-4">
+            {elements.map((element) => (
+              <DesignerElementWrapper key={element.id} element={element} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+    <DesignerSidebar />
+  </div>
 } 
